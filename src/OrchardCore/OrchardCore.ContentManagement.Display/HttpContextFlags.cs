b05269941deb231deea7a5c37ecacb08fe0d0975
Marlon Tucker <marlon.tucker@aareon.com>
2019-03-29 14:51:17 +0000

    Moving HttpContextFlags to ContentManagement.Display

--- src/OrchardCore/OrchardCore.ContentManagement.Display/HttpContextFlags.cs
+++ src/OrchardCore/OrchardCore.ContentManagement.Display/HttpContextFlags.cs
@@ -2,7 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Text;
 
-namespace OrchardCore.ContentManagement
+namespace OrchardCore.ContentManagement.Display
 {
     public static class HttpContextFlags
     {
